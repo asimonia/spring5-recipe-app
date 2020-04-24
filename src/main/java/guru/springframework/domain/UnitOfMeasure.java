fb27c9f9b512afe2da1495b87b4e5d7a6ca78048
Alex Simonian <asimonian@morphotrust.com>
2020-04-24 12:31:39 -0500

    Add data source

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -1,29 +1,31 @@
 package guru.springframework.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class UnitOfMeasure {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
-   private String uom;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
 
-   public Long getId() {
-      return id;
-   }
+    public Long getId() {
+        return id;
+    }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-   public String getUom() {
-      return uom;
-   }
+    public String getDescription() {
+        return description;
+    }
 
-   public void setUom(String uom) {
-      this.uom = uom;
-   }
-
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
